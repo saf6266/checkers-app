23502bef018732dee38b63b4da7ddb5db72c3eea
Shakeel Farooq <shak.farooq2@gmail.com>
2019-10-06 15:57:50 -0400

    Created the PlayerLobby class to represent the list of players already signed in, and added methods for a player to sign in.

---
+++ src/main/java/com/webcheckers/app/PlayerLobby.java
@@ -0,0 +1,28 @@
+package com.webcheckers.app;
+
+import com.webcheckers.model.Player;
+
+import java.util.ArrayList;
+
+public class PlayerLobby {
+
+    private ArrayList<Player> players = new ArrayList<>();
+
+    private boolean checkPlayer(Player player){
+        if(!(players.isEmpty())) {
+            for (Player person : players) {
+                if (person.getName().equals(player.getName()))
+                    return true;    //player is in the list
+            }
+        }
+        return false;   //player is not in the list
+    }
+
+    public boolean addPlayer(Player player){
+        if(!(checkPlayer(player))) {
+            players.add(player);
+            return true;
+        }
+        return false;
+    }
+}
