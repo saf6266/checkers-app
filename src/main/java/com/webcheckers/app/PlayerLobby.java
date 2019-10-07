d994db76dbf925a142c12d66f338d1d6823b24e4
Georgi <gt4330@rit.edu>
2019-10-07 15:31:12 -0400

    Added PlayerLobby methods
    Added Comments

--- src/main/java/com/webcheckers/app/PlayerLobby.java
+++ src/main/java/com/webcheckers/app/PlayerLobby.java
@@ -8,6 +8,11 @@ public class PlayerLobby {
 
     private ArrayList<Player> players = new ArrayList<>();
 
+    /**
+     * This program checks to see if the player's username is already in use
+     * @param player: The current user
+     * @return false if player is not on the list, true if player is on the list
+     */
     private boolean checkPlayer(Player player){
         if(!(players.isEmpty())) {
             for (Player person : players) {
@@ -18,6 +23,11 @@ public class PlayerLobby {
         return false;   //player is not in the list
     }
 
+    /**
+     * Adds a player to current number of players online
+     * @param player: The current user
+     * @return true if the player was added, false if the player name is already in the database
+     */
     public boolean addPlayer(Player player){
         if(!(checkPlayer(player))) {
             players.add(player);
@@ -26,6 +36,19 @@ public class PlayerLobby {
         return false;
     }
 
+    /**
+     * Removes a player from the lobby once they close the page
+     * @param player: The current user
+     */
+    public void removePlayer(Player player){
+        if(!(players.isEmpty()))
+            this.players.remove(player);
+    }
+
+    /**
+     * Get the list of players
+     * @return list of players
+     */
     public ArrayList<Player> getPlayers(){
         return this.players;
     }
