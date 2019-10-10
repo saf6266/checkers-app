b9ba9d5513bfae0d609da9fe2bf544e8cec9fc7d
Shakeel Farooq <shak.farooq2@gmail.com>
2019-10-10 03:30:05 -0400

    Added methods for the inGameList in the gameCenter to add players and determine if players are already in a game.

--- src/main/java/com/webcheckers/app/GameCenter.java
+++ src/main/java/com/webcheckers/app/GameCenter.java
@@ -17,6 +17,23 @@ public class GameCenter {
     private ArrayList<Player> inGameList = new ArrayList<>();
 
     public GameCenter(Player currentUser, Player opponent){
+        board = new BoardView(currentUser, opponent);
+    }
 
+    public BoardView getBoard() {
+        return board;
+    }
+
+    public void addPlayer(Player player){
+        inGameList.add(player);
+    }
+
+    public boolean inGame(Player player){
+        for(Player person : inGameList){
+            if(person.getName().equals(player.getName())){
+                return true;
+            }
+        }
+        return false;
     }
 }
