c1828e5f4112a8948023b282017a6203bd67817b
Shakeel Farooq <shak.farooq2@gmail.com>
2019-11-02 19:40:52 -0400

    Created move methods to check to see if the player move is a valid move or not

--- src/main/java/com/webcheckers/util/Move.java
+++ src/main/java/com/webcheckers/util/Move.java
@@ -5,11 +5,25 @@ public class Move {
     private Position start;
     private Position end;
 
+    public Move(Position start, Position end){
+        this.start = start;
+        this.end = end;
+    }
+
     public Position getStart(){
         return this.start;
     }
 
     public Position getEnd(){
         return this.end;
+    }
+
+    @Override
+    public boolean equals(Object move){
+        if(move instanceof Move){
+            Move other = (Move) move;
+            return this.start == other.start && this.end == other.end;
+        }
+        return false;
     }
 }
