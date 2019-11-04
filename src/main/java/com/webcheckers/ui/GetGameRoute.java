1b4475b26b9f47a13a45b0564cf21ecbf0048b3b
Georgi <gt4330@rit.edu>
2019-11-04 01:03:50 -0500

    GetGameRouteFixed

--- src/main/java/com/webcheckers/ui/GetGameRoute.java
+++ src/main/java/com/webcheckers/ui/GetGameRoute.java
@@ -6,35 +6,22 @@ import com.webcheckers.model.BoardView;
 import com.webcheckers.model.Player;
 import com.webcheckers.model.Row;
 import spark.*;
-import com.google.gson.Gson;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.logging.Logger;
 
-/**
- * The {@code GET /game} route handler.
- */
 public class GetGameRoute implements Route {
-    private static final Logger LOG = Logger.getLogger(GetGameRoute.class.getName());
-    ///
-    ///Attributes
-    ///
+
     private TemplateEngine templateEngine;
     private PlayerLobby playerLobby;
     private GameCenter gameCenter;
 
-    //Values used in the view-model map for rendering the game view
     static final String RED_PLAYER = "redPlayer";
     static final String WHITE_PLAYER = "whitePlayer";
     static final String ACTIVE_COLOR = "activeColor";
-    static final String TITLE = "GAME";
 
-    ///
-    ///Constructor
-    ///
     GetGameRoute(TemplateEngine templateEngine, PlayerLobby playerLobby, GameCenter gameCenter){
         Objects.requireNonNull(templateEngine, "templateEngine is required.");
         Objects.requireNonNull(playerLobby, "playerLobby is required.");
@@ -42,37 +29,21 @@ public class GetGameRoute implements Route {
 
         this.templateEngine = templateEngine;
         this.playerLobby = playerLobby;
-        //
-        LOG.config("GetGameRoute is initialized.");
 
     }
 
-    /**
-     * Render the WebCheckers Game page.
-     *
-     * @param request
-     *   the HTTP request
-     * @param response
-     *   the HTTP response
-     *
-     * @return
-     *   the rendered HTML for the Game page
-     */
     @Override
     public Object handle(Request request, Response response){
-        LOG.finer("GetGameRoute is invoked.");
-
         Map<String, Object> vm = new HashMap<>();
 
-        vm.put("title", TITLE);
+        vm.put("title", "GAME");
 
         final Session session = request.session();
 
         final Player redPlayer = session.attribute(RED_PLAYER);
         final Player whitePlayer = session.attribute(WHITE_PLAYER);
         final Player currentUser = session.attribute(PostSignInRoute.CURR_USER_ATTR);
-        Player.Color activeColor = session.attribute(ACTIVE_COLOR);
-        BoardView board = gameCenter.getBoardView();
+        BoardView board = gameCenter.getBoard();
 
         whitePlayer.setWhite();
         redPlayer.setRed();
@@ -81,7 +52,7 @@ public class GetGameRoute implements Route {
             vm.put(PostSignInRoute.CURR_USER_ATTR, redPlayer);
         }
         else{
-            for(Row row : board.getRows()){ //Flip the board
+            for(Row row : board.getRows()){
                 Collections.reverse(row.getSpaces());
             }
             Collections.reverse(board.getRows());
@@ -92,14 +63,7 @@ public class GetGameRoute implements Route {
 
 
         //Red player goes first
-        vm.put(ACTIVE_COLOR, activeColor);
-
-        //check if game is ended then display this object for game.ftl
-      //  final Map<String, Object> modeOptions = new HashMap<>(2);
-      //  modeOptions.put("isGameOver", true);
-       // modeOptions.put("gameOverMessage", /* get end of game message */);
-       // vm.put("modeOptionsAsJSON", gson.toJson(modeOptions));
-
+        vm.put(ACTIVE_COLOR, Player.Color.RED);
 
         //Set the view Mode as PLAY (for now)
         vm.put("viewMode", GameCenter.Mode.PLAY);
