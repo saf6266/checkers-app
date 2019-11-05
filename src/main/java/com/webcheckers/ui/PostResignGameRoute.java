c6228f3ec335c7dbf628718c6b1273e22f04b913
Shakeel Farooq <shak.farooq2@gmail.com>
2019-11-05 00:12:28 -0500

    Merge branch 'master' of C:\Users\shakf\School\IntroToSoftwareEngineering\Projects\WebCheckers with conflicts.

--- src/main/java/com/webcheckers/ui/PostResignGameRoute.java
+++ src/main/java/com/webcheckers/ui/PostResignGameRoute.java
@@ -1,13 +1,38 @@
 package com.webcheckers.ui;
 
-import spark.Request;
-import spark.Response;
-import spark.Route;
+import com.google.gson.Gson;
+import com.webcheckers.app.GameCenter;
+import com.webcheckers.model.Player;
+import spark.*;
+
+import java.util.Objects;
+import java.util.logging.Logger;
 
 public class PostResignGameRoute implements Route {
+    private static final Logger LOG = Logger.getLogger(PostResignGameRoute.class.getName());
+
+    private TemplateEngine templateEngine;
+    private GameCenter gameCenter;
+    private Gson gson;
+
+    PostResignGameRoute(TemplateEngine templateEngine, GameCenter gameCenter, Gson gson){
+        this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required.");
+        this.gameCenter = Objects.requireNonNull(gameCenter, "gameCenter is required.");
+        this.gson = gson;
+
+        LOG.config("PostResignGameRoute is initialized.");
+    }
 
     @Override
     public Object handle(Request request, Response response){
+        LOG.finer("PostResignGameRoute is invoked");
+
+        final Session session = request.session();
+
+        Player currentUser = session.attribute(PostSignInRoute.CURR_USER_ATTR);
+
+        gameCenter.removePlayer(currentUser);
+
         return null;
     }
 }
