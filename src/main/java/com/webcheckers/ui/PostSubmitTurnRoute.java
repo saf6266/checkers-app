cf29b13786beb1b5022abd91eee8fc7ed1fdd729
42045764 <rathhen123@gmail.com>
2019-11-16 16:45:49 -0500

    removed spacing

--- src/main/java/com/webcheckers/ui/PostSubmitTurnRoute.java
+++ src/main/java/com/webcheckers/ui/PostSubmitTurnRoute.java
@@ -42,8 +42,6 @@ public class PostSubmitTurnRoute implements Route {
             } else {
                 gameCenter.getBoardView().setActivecolor(Player.Color.RED);
 
-
-
             }
 
             BoardView mostRecent = gameCenter.getStackOfBoardView().peek();
